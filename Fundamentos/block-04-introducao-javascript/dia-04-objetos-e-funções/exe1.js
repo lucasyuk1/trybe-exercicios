c7038276857a534b49916ea04c71cc8d2ec8e930
LucasCS <lcsvdk@gmail.com>
2022-09-26 18:13:13 -0300

    Exercicios de for in

---
+++ Fundamentos/block-04-introducao-javascript/dia-04-objetos-e-funções/exe1.js
@@ -0,0 +1,26 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+// Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Use a sintaxe meuObjeto.chave. Valor esperado no console:
+
+console.log('Bem vindo(a),', info.personagem);
+
+//Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. Use a sintaxe meuObjeto['chave'] = valor. Valor esperado no console:
+
+info['recorrente'] = 'Sim';
+
+console.log(info);
+
+// Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+
+for (const key in info) {
+  console.log(key);
+}
+
+// Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+
+for (let key in info) {
+  console.log(info[key]);
+}
