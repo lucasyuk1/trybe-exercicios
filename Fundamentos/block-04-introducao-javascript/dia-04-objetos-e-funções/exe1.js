6f7523eacba0db6ee6f69a08ee3eccbbbc4c56e9
LucasCS <lcsvdk@gmail.com>
2022-09-26 18:38:36 -0300

    Exercicios de objetos encadeados

--- Fundamentos/block-04-introducao-javascript/dia-04-objetos-e-funções/exe1.js
+++ Fundamentos/block-04-introducao-javascript/dia-04-objetos-e-funções/exe1.js
@@ -24,3 +24,16 @@ for (const key in info) {
 for (let key in info) {
   console.log(info[key]);
 }
+
+// Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. Valor esperado no console:
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (let key in info) {
+  console.log(info[key],' e ',info2[key]);
+}
